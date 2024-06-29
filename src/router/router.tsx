d36c5bffdeabaa5d01585c43c55fd1f8943652b3
JIHU96 <korjihu@gmail.com>
2024-06-29 11:47:21 +0900

    route: 필요한 페이지들 생성 & router 파일에 연결

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import { memo } from "react";
 import Error from "../views/Error";
 import Index from "../views/Index";
+import Fashion from "../views/Fashion";
+import Accessory from "../views/Accessory";
+import Digital from "../views/Digital";
+import Cart from "../views/Cart";
+import Product from "../views/Product";
 
 const Router = (): JSX.Element => {
   return (
@@ -9,6 +14,11 @@ const Router = (): JSX.Element => {
       <Route path="*" element={<Error />} />
       <Route path="/" element={<Index />} />
       {/* 라우팅 추가 해보세요. */}
+      <Route path="/fashion" element={<Fashion />} />
+      <Route path="/accessory" element={<Accessory />} />
+      <Route path="/digital" element={<Digital />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/product/:id" element={<Product />} />
     </Routes>
   );
 };
