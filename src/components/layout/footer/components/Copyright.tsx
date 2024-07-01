0aa590d6ceae88a1519a99262b29568f2bd91cb3
JIHU96 <korjihu@gmail.com>
2024-07-02 01:50:31 +0900

    feat: Copyright 컴포넌트 간단 수정

--- src/components/layout/footer/components/Copyright.tsx
+++ src/components/layout/footer/components/Copyright.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Copyright = () => {
-  return <div>Copyright</div>;
+  return (
+    <div>
+      <p>Copyright © 2024 Zero Base</p>
+    </div>
+  );
 };
 
 export default Copyright;
