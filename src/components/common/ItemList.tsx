d98cbc61e3ca5641d6e6dc755550b7d4ce275810
JIHU96 <korjihu@gmail.com>
2024-07-02 16:32:04 +0900

    fix: 내부에서 분리한 컴포넌트 제거하여 return에 바로 적용

--- src/components/common/ItemList.tsx
+++ src/components/common/ItemList.tsx
@@ -21,28 +21,14 @@ interface ItemListProps {
 
 // 메인페이지에서 카테고리에 맞는 대표 아이템을 보여줌
 const ItemList = ({ category, items }: ItemListProps) => {
-  //
-  const Title = () => {
-    return <h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">{MENUS[category]}</h2>;
-  };
-
-  //
-  const ItemWrapper = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list" data-scroll="true">
-        {children}
-      </div>
-    );
-  };
-
   return (
     <>
-      <Title />
-      <ItemWrapper>
+      <h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">{MENUS[category]}</h2>
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list" data-scroll="true">
         {items.map((item) => (
           <Item key={item.id} item={item} />
         ))}
-      </ItemWrapper>
+      </div>
     </>
   );
 };
