8c054b1a200aa7c4cdbb5f4cdd708ef2ad304b1d
JIHU96 <korjihu@gmail.com>
2024-07-02 22:58:29 +0900

    feat: 웹페이지 메인에 접속 시, API 통신으로 제품들 가져오는 코드 추가

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,27 +1,42 @@
+import { Suspense, lazy } from "react";
 import Slider from "../components/common/Slider";
-import ItemList from "@/components/common/ItemList";
-import products from "@public/products.json";
+import useGetItems from "@/store/useGetItems";
+import ProductsLoad from "@/components/products/ProductsLoad";
+const ItemList = lazy(() => import("@/components/common/ItemList"));
+
+const API_URL = "https://fakestoreapi.com/products";
 
 const Index = (): JSX.Element => {
-  const product = JSON.parse(JSON.stringify(products));
-  const fashionItems = [product[0], product[1], product[2], product[3]];
-  const accessoryItems = [product[4], product[5], product[6], product[7]];
-  const digitalItems = [product[8], product[9], product[10], product[11]];
+  const { data, loading, error } = useGetItems(API_URL);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <>
       <Slider />
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <ItemList category="FASHION" items={fashionItems} />
+        <Suspense fallback={<ProductsLoad limit={4} />}>
+          <ItemList category="FASHION" items={[data[0], data[1], data[2], data[3]]} />
+        </Suspense>
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <ItemList category="ACCESSORY" items={accessoryItems} />
+        <Suspense fallback={<ProductsLoad limit={4} />}>
+          <ItemList category="ACCESSORY" items={[data[4], data[5], data[6], data[7]]} />
+        </Suspense>
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mb-20 xl:container mx-auto">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <ItemList category="DIGITAL" items={digitalItems} />
+        <Suspense fallback={<ProductsLoad limit={4} />}>
+          <ItemList category="DIGITAL" items={[data[8], data[9], data[10], data[11]]} />
+        </Suspense>
       </section>
     </>
   );
