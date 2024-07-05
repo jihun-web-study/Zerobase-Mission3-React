da1b691324c9e1d979727c3f1b31c5a7704e0661
JIHU96 <korjihu@gmail.com>
2024-07-05 20:12:07 +0900

    feat: Nav 컴포넌트의 카트 수량에 카트리스트 연결

--- src/components/layout/nav/components/NavCart.tsx
+++ src/components/layout/nav/components/NavCart.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { cartCount } from "@/store/cart";
 import { Link } from "react-router-dom";
+import { useRecoilValueLoadable } from "recoil";
 
 const NavCart = () => {
-  const [items, setItems] = useState(0);
+  const { state, contents: cartCounts } = useRecoilValueLoadable(cartCount);
+
+  if (state === "loading") return <div>1</div>;
 
   return (
     <Link className="btn btn-ghost w-10 sm:w-12 ml-1" to="/cart">
@@ -21,7 +24,7 @@ const NavCart = () => {
           ></path>
         </svg>
         <span className="inline-flex items-center justify-center absolute top-0 right-0 px-2 py-1 rounded-full bg-red-500 text-xs font-bold leading-none text-gray-200 transform translate-x-1/2 -translate-y-1/2">
-          {items}
+          {state === "hasValue" ? cartCounts : 0}
         </span>
       </span>
     </Link>
