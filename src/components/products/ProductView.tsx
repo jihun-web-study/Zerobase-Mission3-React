91ec24f3b2d822727f3fc5e826417577e9cc7e3f
JIHU96 <korjihu@gmail.com>
2024-07-05 19:58:11 +0900

    feat: 장바구니 담기 기능 추가

--- src/components/products/ProductView.tsx
+++ src/components/products/ProductView.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import Rating from "@/components/common/Rating";
 import { IProduct } from "@/store/products";
 import { Link } from "react-router-dom";
 import { toCurrencyFormat } from "@/helpers/helpers";
+import { addToCart, cartState, ICartState } from "@/store/cart";
+import { useRecoilState } from "recoil";
 
-interface ProductViewProps {
-  product: IProduct;
-}
+const ProductView = ({ product }: { product: IProduct }) => {
+  const [cart, setCart] = useRecoilState<ICartState>(cartState);
 
-const ProductView = ({ product }: ProductViewProps) => {
   const ViewImage = () => {
     return (
       <figure className="flex-shrink-0 rounded-2xl overflow-hidden px-16 py-4 bg-white view_image">
@@ -30,18 +29,20 @@ const ProductView = ({ product }: ProductViewProps) => {
   };
 
   const CardPrice = () => {
-    return <p className="mt-2 mb-4 text-3xl">${toCurrencyFormat(product.price)}</p>;
+    return <p className="mt-2 mb-4 text-3xl">{toCurrencyFormat(product.price)}</p>;
   };
 
   const CardActions = () => {
-    // 장바구니 담기 로직 추가 해야함
-    const onClickHandler = () => {
-      console.log("장바구니 담기 클릭");
-    };
-
     return (
       <div className="card-actions">
-        <button className="btn btn-primary" onClick={onClickHandler}>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            console.log("장바구니 담기 클릭");
+            const newCart = addToCart(cart, String(product.id));
+            setCart(newCart);
+          }}
+        >
           장바구니에 담기
         </button>
         <Link className="btn btn-outline ml-1" to={"/cart"}>
