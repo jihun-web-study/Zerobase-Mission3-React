d8b57d54affbe6ce9fad5236f3c72be2fd3bc034
JIHU96 <korjihu@gmail.com>
2024-07-05 14:07:38 +0900

    fix: recoilValue의 동기적 이슈 해결 &  Suspense 동작 잘 안하는 이슈 수정

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,35 +1,42 @@
-import { Suspense, lazy } from "react";
+import { lazy, Suspense } from "react";
 import Slider from "../components/common/Slider";
 import ProductsLoad from "@/components/products/ProductsLoad";
-import { useRecoilValue } from "recoil";
+import { useRecoilValueLoadable } from "recoil";
 import { mainProductsList } from "@/store/products";
-
-const ItemList = lazy(() => import("@/components/common/ItemList"));
+import ItemList from "@/components/common/ItemList";
+//const ItemList = lazy(() => import("@/components/common/ItemList"));
 
 const Index = (): JSX.Element => {
-  const representProducts = useRecoilValue(mainProductsList);
-  console.log("representProducts: ", representProducts);
+  const { state, contents: representProducts } = useRecoilValueLoadable(mainProductsList);
+  console.log("representProducts: ", state, representProducts);
 
+  // Suspnese가 동작안함 + useRecoildValue의 동기적 실행으로 인해 마운트전 렌더링 에러 발생
+  // => useRecoilValueLoadable의 state로 분기함
   return (
     <>
       <Slider />
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <Suspense fallback={<ProductsLoad limit={4} />}>
-          <ItemList category="FASHION" items={representProducts.fashion} />
-        </Suspense>
+        {state === "loading" && <ProductsLoad limit={4} />}
+        {state === "hasValue" && <ItemList category="FASHION" items={representProducts.fashion} />}
+        {/* <Suspense fallback={<ProductsLoad limit={4} />}>
+          {state === "hasValue" && <ItemList category="FASHION" items={representProducts.fashion} />}
+        </Suspense> */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <Suspense fallback={<ProductsLoad limit={4} />}>
-          <ItemList category="ACCESSORY" items={representProducts.accessory} />
-        </Suspense>
+        {state === "loading" && <ProductsLoad limit={4} />}
+        {state === "hasValue" && <ItemList category="FASHION" items={representProducts.accessory} />}
+        {/* <Suspense fallback={<ProductsLoad limit={4} />}>
+          {state === "hasValue" && <ItemList category="FASHION" items={representProducts.accessory} />}
+        </Suspense> */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mb-20 xl:container mx-auto">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        {state === "loading" && <ProductsLoad limit={4} />}
+        {state === "hasValue" && <ItemList category="FASHION" items={representProducts.digital} />}
         <Suspense fallback={<ProductsLoad limit={4} />}>
-          {" "}
-          <ItemList category="DIGITAL" items={representProducts.digital} />
+          {state === "hasValue" && <ItemList category="FASHION" items={representProducts.digital} />}
         </Suspense>
       </section>
     </>
